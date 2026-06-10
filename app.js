1d36434610a5f8e141a9b89f203fe2b914ada962
RishuKumar09 <34654777+RishuKumar09@users.noreply.github.com>
2026-06-10 21:20:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var port = process.env.PORT || 3000,
+var port = process.env.PORT || 8080,   // ✅ Fix port
     http = require('http'),
     fs = require('fs'),
     html = fs.readFileSync('index.html');
@@ -18,8 +18,9 @@ var server = http.createServer(function (req, res) {
         req.on('end', function() {
             if (req.url === '/') {
                 log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+            } else if (req.url === '/scheduled') {   // ✅ Fix
+                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + 
+                ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
             }
 
             res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
@@ -32,8 +33,7 @@ var server = http.createServer(function (req, res) {
     }
 });
 
-// Listen on port 3000, IP defaults to 127.0.0.1
-server.listen(port);
-
-// Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:' + port + '/');
+// ✅ VERY IMPORTANT FIX
+server.listen(port, '0.0.0.0', function() {
+    console.log('Server running on port ' + port);
+});
